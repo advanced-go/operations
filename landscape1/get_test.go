f320a3b0637f339515abcce44e238544822fd9bc
advanced-go <mark.wilson.go@outlook.com>
2024-06-28 07:26:07 -0500

    Updating entry data

--- landscape1/get_test.go
+++ landscape1/get_test.go
@@ -11,12 +11,12 @@ func ExampleGet() {
 	entries, _, status := get[core.Output, Entry](nil, nil, uri.BuildValues(q), partitionResource, "", nil)
 	fmt.Printf("test: get(\"%v\") -> [status:%v] [entries:%v]\n", q, status, len(entries))
 
-	q = "assigned-region=west&order=desc"
+	q = "assigned-region=west&status=active&traffic=ingress"
 	entries, _, status = get[core.Output, Entry](nil, nil, uri.BuildValues(q), partitionResource, "", nil)
 	fmt.Printf("test: get(\"%v\") -> [status:%v] [entries:%v]\n", q, status, entries)
 
 	//Output:
 	//test: get("status=active") -> [status:OK] [entries:6]
-	//test: get("assigned-region=west&order=desc") -> [status:OK] [entries:[{3 us-south1 c  ingress active  2024-06-10 09:00:35 +0000 UTC ingress-case-class1 west} {2 us-west1 b  egress active  2024-06-10 09:00:35 +0000 UTC egress-case-class1 west} {1 us-west1 a  ingress active  2024-06-10 09:00:35 +0000 UTC ingress-case-class1 west}]]
-
+	//test: get("assigned-region=west&status=active&traffic=ingress") -> [status:OK] [entries:[{1 us-west1 a  ingress active  2024-06-10 09:00:35 +0000 UTC ingress-case-class1 west} {5 us-central1 c  ingress active  2024-06-10 09:00:35 +0000 UTC ingress-case-class1 west}]]
+	
 }
