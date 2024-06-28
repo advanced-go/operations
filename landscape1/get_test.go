cbc3b92cd4aafa0189763b150283b9cf8c96dc72
advanced-go <mark.wilson.go@outlook.com>
2024-06-28 07:35:25 -0500

    Update get_test.go

--- landscape1/get_test.go
+++ landscape1/get_test.go
@@ -18,5 +18,5 @@ func ExampleGet() {
 	//Output:
 	//test: get("status=active") -> [status:OK] [entries:6]
 	//test: get("assigned-region=west&status=active&traffic=ingress") -> [status:OK] [entries:[{1 us-west1 a  ingress active  2024-06-10 09:00:35 +0000 UTC ingress-case-class1 west} {5 us-central1 c  ingress active  2024-06-10 09:00:35 +0000 UTC ingress-case-class1 west}]]
-	
+
 }
